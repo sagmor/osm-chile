17b81c77aa0d81f29b713d7f0f3bb98d5f05af01
Vijay Khemlani <vkhemlan@gmail.com>
2010-08-21 11:51:36 -0400

    Spike solution de web-maps-lite

---
+++ spikes/javascripts/script.js
@@ -0,0 +1,3 @@
+var cloudmade = new CM.Tiles.CloudMade.Web({key: 'E282E0EA16754AFB98173BF9A17CF874'});
+var map = new CM.Map('cm-example', cloudmade);
+map.setCenter(new CM.LatLng(-33.437833, -70.650333), 15);
