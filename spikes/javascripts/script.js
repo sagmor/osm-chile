349966de650ee4928dfa0f71e25172b4b5eb7cae
Sebastián Gamboa <me@sagmor.com>
2010-09-18 12:24:50 -0400

    Cleaning up.

--- spikes/javascripts/script.js
+++
@@ -1,3 +0,0 @@
-var cloudmade = new CM.Tiles.CloudMade.Web({key: 'E282E0EA16754AFB98173BF9A17CF874'});
-var map = new CM.Map('cm-example', cloudmade);
-map.setCenter(new CM.LatLng(-33.437833, -70.650333), 15);
